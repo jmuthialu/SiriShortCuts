9feaf8dc638a0220e1dbd30db08e46f779e9d439
Jay Muthialu <jay.muthialu@turner.com>
2018-06-21 09:03:42 -0400

    UI changes

--- BlackCoffee/AppDelegate.swift
+++ BlackCoffee/AppDelegate.swift
@@ -15,6 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        INPreferences.requestSiriAuthorization { status in
+            if status == .authorized {
+                print("Hey, Siri!")
+            } else {
+                print("No, Siri!")
+            }
+        }
+        return true
+    }
+    
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         
         if let intent = userActivity.interaction?.intent as? OrderCoffeeIntent {
--- IntentHandlerUI/IntentViewController.swift
+++ IntentHandlerUI/IntentViewController.swift
@@ -8,7 +8,7 @@
 
 import IntentsUI
 
-class IntentViewController: UIViewController, INUIHostedViewControlling {
+class IntentViewController: UIViewController, INUIHostedViewControlling, INUIHostedViewSiriProviding {
     
     @IBOutlet var confirmationView: ConfirmationView!
     @IBOutlet var finalizeView: FinalizeView!
@@ -21,12 +21,22 @@ class IntentViewController: UIViewController, INUIHostedViewControlling {
     
     // Prepare your view controller for the interaction to handle.
     func configureView(for parameters: Set<INParameter>, of interaction: INInteraction, interactiveBehavior: INUIInteractiveBehavior, context: INUIHostedViewContext, completion: @escaping (Bool, Set<INParameter>, CGSize) -> Void) {
-        
+
         for view in view.subviews {
             view.removeFromSuperview()
         }
-        
-        if interaction.intentHandlingStatus == .ready {
+
+        if interaction.intentHandlingStatus == .unspecified { // from siri. This is not correct way to handle
+            view.addSubview(confirmationView)
+            if let intent = interaction.intent as? OrderCoffeeIntent, let productName = intent.productName {
+                confirmationView.productName.text = "Qty of \(productName)" // Unspecified
+                confirmationView.productImageView.image = UIImage(named: productName)
+                if let qtyNSNumber = intent.quantity {
+                    let qtyInt = Int(truncating: qtyNSNumber)
+                    confirmationView.quantity.text = String(qtyInt)
+                }
+            }
+        } else if interaction.intentHandlingStatus == .ready {
             view.addSubview(confirmationView)
             if let intent = interaction.intent as? OrderCoffeeIntent, let productName = intent.productName {
                 confirmationView.productName.text = "Qty of \(productName)"
@@ -40,12 +50,12 @@ class IntentViewController: UIViewController, INUIHostedViewControlling {
             view.addSubview(finalizeView)
             finalizeView.price.text = "$10"
         }
-        
+
         completion(true, parameters, self.desiredSize)
     }
-    
+
     var desiredSize: CGSize {
-        return CGSize(width: 320, height: 150)
+        return CGSize(width: 320, height: 100)
     }
     
 }
